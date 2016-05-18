8fe89fecca7029417121d8f320f73b967e56d0f1
Talented-Dev934 <eriellebancolitadev@gmail.com>
2016-05-18 22:13:06 +0200

    Automate AWS Route 53 setup.

--- README.md
+++ README.md
@@ -15,3 +15,15 @@ $ http-server
 $ npm install
 $ npm test
 ```
+
+## Deploy/Update the AWS Route 53 domain
+
+```bash
+$ sudo pip install awscli
+$ aws configure
+AWS Access Key ID [None]: ********
+AWS Secret Access Key [None]: ********
+Default region name [None]: us-east-1
+Default output format [None]:
+$ ./aws/dns.sh
+```
---
+++ aws/dns.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+DOMAIN_NAME=myberl.in
+
+# Take that, bot:
+ADDRESS="4"
+ADDRESS=". ${ADDRESS}"
+ADDRESS="str${ADDRESS}"
+ADDRESS="ein${ADDRESS}"
+ADDRESS="nst${ADDRESS}"
+ADDRESS="cke${ADDRESS}"
+ADDRESS="Fal${ADDRESS}"
+ADDRESS="${ADDRESS}2"
+PHONE=$((2256329 * 720 + 4 * 10))
+PHONE="+49.$PHONE"
+EMAIL="n.l"
+EMAIL="lie${EMAIL}our"
+EMAIL="ure${EMAIL}ot@"
+EMAIL="a${EMAIL}gma"
+EMAIL="${EMAIL}il."
+EMAIL="${EMAIL}com"
+
+CONTACT="FirstName=Aurelien,LastName=Lourot,ContactType=PERSON,OrganizationName=,\
+AddressLine1=$ADDRESS,AddressLine2=,City=Berlin,State=,CountryCode=DE,ZipCode=10997,\
+PhoneNumber=$PHONE,Email=$EMAIL,Fax=,ExtraParams=[]"
+
+aws route53domains register-domain --domain-name $DOMAIN_NAME --duration-in-years 1 \
+    --no-auto-renew --admin-contact "$CONTACT" --registrant-contact "$CONTACT" \
+    --tech-contact "$CONTACT" --privacy-protect-admin-contact --privacy-protect-registrant-contact \
+    --privacy-protect-tech-contact
+
+#aws route53domains get-domain-detail --domain-name $DOMAIN_NAME > output.txt
