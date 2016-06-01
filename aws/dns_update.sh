5adf12276b8cb055ee980834cb5d37539cb0539f
Talented-Dev934 <eriellebancolitadev@gmail.com>
2016-06-01 23:00:44 +0200

    www subdomain created.

--- aws/dns_update.sh
+++ aws/dns_update.sh
@@ -9,6 +9,8 @@ aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID \
     --change-batch "file://$scriptDir/dns_upsert_a_record_set.json"
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID \
     --change-batch "file://$scriptDir/dns_upsert_a_kloudsec_record_set.json"
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID \
+    --change-batch "file://$scriptDir/dns_upsert_cname_www_record_set.json"
 
 #aws route53 list-hosted-zones > output.txt
 #aws route53 get-hosted-zone --id $HOSTED_ZONE_ID > output.txt
