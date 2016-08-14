b2e0c1812d6348130260e3c226370f064f8f3bb4
Talented-Dev934 <eriellebancolitadev@gmail.com>
2016-08-14 11:29:35 +0200

    Monitor Romu's map.

--- snapshots/maps/thirdparty/download.sh
+++ snapshots/maps/thirdparty/download.sh
@@ -4,6 +4,11 @@ set -e
 
 # Call this script regularly to save the external maps we rely on.
 
-for map_id in 1S_guQBQudaPzIgRybK3VIPYwRL0 1RklTwBGE0Ke7c_-n-ONFAP-5kV0; do
+#FIXME: generate this list
+maps="1S_guQBQudaPzIgRybK3VIPYwRL0 \
+      1RklTwBGE0Ke7c_-n-ONFAP-5kV0 \
+      1NXPVu6MaJ7ASWCmSOe27oXPF5rc"
+
+for map_id in $maps; do
   curl -o "${map_id}.kml" "https://www.google.com/maps/d/kml?mid=${map_id}&forcekml=1"
 done
