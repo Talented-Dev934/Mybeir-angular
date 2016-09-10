889059bbbd757b63470cb66f5605ee027fc98613
Talented-Dev934 <eriellebancolitadev@gmail.com>
2016-09-10 16:40:21 +0200

    Fix missing String.startsWith() in PhantomJS.

--- thirdparty/polyfills.js
+++ thirdparty/polyfills.js
@@ -101,3 +101,11 @@ if (!String.prototype.repeat) {
     return rpt;
   }
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith#Polyfill
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+  };
+}
