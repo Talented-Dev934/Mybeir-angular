fde764b8ec520c1891a75c7347533238c68024bd
Talented-Dev934 <eriellebancolitadev@gmail.com>
2016-09-12 23:57:06 +0200

    Increase Jasmine timeout.

--- test/e2e.js
+++ test/e2e.js
@@ -5,6 +5,8 @@ requirejs.config({
 });
 
 describe('app', function() {
+  var originalTimeout;
+
   beforeEach(function() {
     var fixture
       = '           \
@@ -16,6 +18,11 @@ describe('app', function() {
 </style>            \
 <div id="fixture"><map on-tag-click=""></map><tag-filters></tag-filters></div>';
     document.body.insertAdjacentHTML('afterbegin', fixture);
+
+    // On Firefox 45.0 the app gets easily ready within 5 seconds but on Travis CI they use Firefox
+    // 31.0 which is slower:
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
   });
 
   it('should get ready', function(done) {
@@ -27,6 +34,9 @@ describe('app', function() {
   });
 
   afterEach(function() {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+
+    //FIXME: we should remove <style> as well:
     document.body.removeChild(document.getElementById('fixture'));
   });
 });
