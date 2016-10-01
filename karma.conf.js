8bb4856e78974f254efb432652e02325161cf911
Talented-Dev934 <eriellebancolitadev@gmail.com>
2016-10-01 18:24:29 +0200

    Fix "Module 'ngAnimate' is not available".

--- karma.conf.js
+++ karma.conf.js
@@ -30,9 +30,9 @@ module.exports = function(config) {
       'thirdparty/geolocation-marker.js',
       'thirdparty/x2js-1.2.0/xml2json.min.js',
       'thirdparty/angularjs-1.4.4/angular.min.js',
+      'thirdparty/angularjs-1.4.4/angular-animate.min.js',
       'thirdparty/require.js',
       'thirdparty/animated-modal-1.0/animatedModal.min.js',
-      'thirdparty/angularjs-1.4.4/angular-animate.min.js',
       'http://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular-mocks.js',
 
       // The order matters, as mocked angular modules can't be unmocked afterwards. Thus when a test
