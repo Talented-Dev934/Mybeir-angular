34da3d1c773c73c1aa568eec6a07a481949d91e4
Talented-Dev934 <eriellebancolitadev@gmail.com>
2017-07-09 16:15:31 +0300

    Tags edited, existing markers refined, markers added.

--- angular/map/marker-patches.js
+++ angular/map/marker-patches.js
@@ -6,7 +6,14 @@ define({
 
 // Patches the provided dictionary of instances of googlemap.Marker.
 function patch(markers) {
-  // No patches yet.
+  setPlaceId('emnazih', 'ChIJmf6F__kWHxUR6wQO2krgwgI');
+  setPlaceId('jammal', 'ChIJ-YEAENJYHxUR-e4EVMcfJpo');
+  setPlaceId('kahwetleila', 'ChIJ84X1_PoWHxURYD7ooECcnvA');
+  setPlaceId('lazybbeach', 'ChIJ6XMwtx3jHhUR4rFiK5R3AS4');
+  setPlaceId('londonbar', 'ChIJT0k2-VgWHxURzHHHNL6iqe4');
+  setPlaceId('mayrig', 'ChIJkXEyffcWHxURgxjedHI0SUA');
+  setPlaceId('naturalbridge', 'ChIJyVZhcZ9OHxURJjMW5igiIyk');
+  setPlaceId('seza', 'ChIJj6eAWCEXHxURPpItJi93dpg');
 
   function setComment(id, comment) {
     try {
